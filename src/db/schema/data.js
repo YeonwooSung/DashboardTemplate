9c8d021d1fef24ff580b7c6cb8f9a427c845f1f0
YeonwooSung <neos960518@gmail.com>
2020-08-15 14:46:58 +0900

    Update data schema

--- src/db/schema/data.js
+++ src/db/schema/data.js
@@ -5,6 +5,9 @@ var dataSchema = new Schema({
     id: String,     // user id
     type: String,   // i.e. 'c', 't', 'h'
     value: Number,  // value
+    serial: Number, // sequential value
+    ip: String,     // ip address of sender device
+    node: String,   // node information
     timestamp: {
         type: Date,
         default: Date.now
